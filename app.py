a34422905cad7f3027091cb92bd7abcee01c0559
rahul1205 <anand.rahul1205@gmail.com>
2020-04-11 07:02:31 +1200

    APIs complete

---
+++ app.py
@@ -0,0 +1,44 @@
+import os
+import mysql.connector
+import requests
+from flask import Flask, jsonify, request
+from flask_material import Material
+
+sql_db = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="root",
+  database="TRS"
+)
+
+cursor = sql_db.cursor()
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+Material(app)
+
+@app.route('/destinations', methods=['GET', 'POST'])
+def destinations():
+    keyword = request.args.get('keyword', '')
+    print (keyword)
+    query = 'SELECT * from booking_destinations where name LIKE %s'
+    try:
+        cursor.execute(query, ("%" + keyword + "%",))
+        result_sql = cursor.fetchall()
+        return jsonify(result_sql)
+    except Exception as e:
+        # print (str(e))
+        return jsonify([])
+
+
+@app.route('/destination_by_id', methods=['GET', 'POST'])
+def destination_by_id():
+    idn = request.args.get('id')
+    query = 'SELECT * from booking_destinations where id = ' + str(idn)
+    try:
+        cursor.execute(query)
+        result_sql = cursor.fetchall()
+        return jsonify(result_sql)
+    except Exception as e:
+        print (str(e))
+        return jsonify("Some Exception Occured")
